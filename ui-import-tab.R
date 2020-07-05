b3fb3fcb10b1b174655e367da743b72ab3533d65
Miriam <fernando.perez8@um.es>
2020-07-05 17:32:38 +0200

    some comments

--- ui-import-tab.R
+++ ui-import-tab.R
@@ -1,9 +1,24 @@
 fluidPage(
     fluidRow(
         column(width = 4,
+               circleButton(
+                   inputId = "infoTEST",
+                   icon = icon("info"),
+                   size = "xs",
+                   status = "primary"
+               ),
+               bsTooltip(
+                   "infoTEST",
+                   paste0("Select Wald's test or Likelihood Ratio Test.",
+                          "Wald's test performs pairwise test using first category as reference.",
+                          "LTR performs comparison between full and reduced model"),
+                   trigger = "hover",
+                   placement = "left"
+               ),
                box(width = 12,
                 uiOutput("testVariable"),
                 htmlOutput("textTestAlgorithm"),
+                
                 uiOutput("testAlgorithm"),
                 uiOutput("testButton")
                )
